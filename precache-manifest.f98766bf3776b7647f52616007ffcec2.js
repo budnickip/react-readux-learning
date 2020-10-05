48696edd0e318c394f59310030a2a160c0cc57ba
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-10-05 16:23:43 +0200

    Updates

---
+++ precache-manifest.f98766bf3776b7647f52616007ffcec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbac73858bf18d333a5bf050060a0f6c",
+    "url": "/react-readux-learning/index.html"
+  },
+  {
+    "revision": "a9e55fef6f92450de30a",
+    "url": "/react-readux-learning/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7cfea6aae8be1e85693e",
+    "url": "/react-readux-learning/static/js/2.3a8887ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-readux-learning/static/js/2.3a8887ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e55fef6f92450de30a",
+    "url": "/react-readux-learning/static/js/main.a1e68c3b.chunk.js"
+  },
+  {
+    "revision": "208570744eb64556c65d",
+    "url": "/react-readux-learning/static/js/runtime-main.1c02254e.js"
+  }
+]);
